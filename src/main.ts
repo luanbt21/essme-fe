76f43ec61c1cd1094f0e0f3ac5513c9c877e18eb
luanbt21 <luanbt21@gmail.com>
2022-01-06 21:55:57 +0700

    config axios baseurl

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
+import axios from 'axios'
 
 import 'element-plus/dist/index.css'
 import '~/styles/index.scss'
@@ -9,10 +10,10 @@ import { store, key } from './store'
 
 import App from './App.vue'
 
+axios.defaults.baseURL = 'https://essme.duckdns.org/api/'
+
 const app = createApp(App)
 // app.use(ElementPlus)
 app.use(store, key)
 app.use(router)
 app.mount('#app')
-
-const elem = document.getElementById("defineAsyncComponent");
