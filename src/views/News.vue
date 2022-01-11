eb48a4b9517b9e18f1d40c4334d41de927414158
luanbt21 <luanbt21@gmail.com>
2022-01-11 21:24:40 +0700

    add news page

---
+++ src/views/News.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container mx-auto">
+    <div v-for="news in newsArr" :key="news._id">
+      <NewsItem :news="news" />
+    </div>
+    <div class="text-center">
+      <el-pagination layout="prev, pager, next" :page-size="20" :total="1000" @current-change="handleCurrentChange" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { getNews } from '~/api/News'
+import NewsItem from '~/components/NewsItem.vue'
+import { News } from '~/models/News'
+
+const newsArr = ref<News[]>([])
+const page = ref(1)
+
+const handleCurrentChange = (val: number) => {
+  page.value = val
+}
+
+onMounted(async () => {
+  newsArr.value = await getNews(20)
+})
+</script>
