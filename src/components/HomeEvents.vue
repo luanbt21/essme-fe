a7a26a3120b8f8b6b4c86ce562360ad4a357eb51
Duonghuong01 <86471650+Duonghuong01@users.noreply.github.com>
2022-01-12 16:33:45 +0700

    commit lai HomeEvents

--- src/components/HomeEvents.vue
+++ src/components/HomeEvents.vue
@@ -3,19 +3,21 @@
     <div class="px-8 py-4 bg-[#D1E0DB] rounded-xl">
       <h2 class="text-center text-2xl mb-2">Events</h2>
       <el-row :gutter="30">
-        <el-col v-for="events in newsArr" :key="events._id" :xs="24" :sm="12" :lg="8">
+        <el-col v-for="events in newsArr.slice(2, 8)" :key="events._id" :xs="24" :sm="12" :lg="8">
           <a :href="events.web" target="_blank">
             <el-card class="h-40 mb-5 md:hover:scale-105 hover:duration-500" :body-style="{ padding: '10px' }">
               <div class="flex flex-row h-full">
-                <img :src="events.img" alt="" class="w-56 h-auto rounded-lg mr-3" />
-                <div>
-                  <h4 class="line-clamp-2 h-fit min-h-[3rem]">
+                <div class="mr-3">
+                  <img :src="events.img" alt="" class="w-80 h-32 sm:w-44 md:w-80 lg:w-96 xl:w-80 rounded-lg mt-1" />
+                </div>
+                <div class="pb-9">
+                  <h4 class="line-clamp-2 h-fit min-h-fit">
                     <b>{{ events.event_name }}</b>
                   </h4>
-                  <p class="line-clamp-4 text-sm h-fit">
+                  <p class="line-clamp-4 text-sm h-fit mt-5">
                     {{ events.time }}
                   </p>
-                  <p class="line-clamp-4 text-sm h-fit">Địa điểm: {{ events.location }}</p>
+                  <p class="line-clamp-4 text-sm h-fit mb-10">Địa điểm: {{ events.location }}</p>
                 </div>
               </div>
             </el-card>
@@ -39,6 +41,6 @@ import { Events } from '~/models/Events'
 const newsArr = ref<Events[]>([])
 
 onMounted(async () => {
-  newsArr.value = await getEvents(6, 0)
+  newsArr.value = await getEvents(20, 0)
 })
 </script>
