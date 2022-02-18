242fb4a2343a4fd91d06d63eb4e4ec03532ab3d5
imgiang <hoahonggai1199@gmail.com>
2022-02-19 05:54:28 +0700

    api home Expert (change)

--- src/components/HomeExpert.vue
+++ src/components/HomeExpert.vue
@@ -3,7 +3,15 @@
     <div class="px-8 py-4 bg-[#D1E0DB] rounded-lg" style="padding-left: 5%; padding-right: 5%">
       <h1 class="text-center text-4xl mb-10">Experts</h1>
       <el-row :gutter="0">
-        <el-col class="bg-[#D1E0DB]" v-for="expert in expertArr" :key="expert._id" :xs="12" :sm="12" :md="8" :lg="6">
+        <el-col
+          class="bg-[#D1E0DB] col"
+          v-for="expert in expertArr"
+          :key="expert._id"
+          :xs="12"
+          :sm="12"
+          :md="8"
+          :lg="6"
+        >
           <el-popover
             style="border-radius: 10px"
             trigger="hover"
@@ -15,7 +23,7 @@
               <el-card
                 @click="getIdExpert(expert._id)"
                 :body-style="{ padding: '0px' }"
-                class="mb-10 background-color-radius"
+                class="elcard mb-10 background-color-radius"
               >
                 <router-link to="/expert" custom v-slot="{ navigate, href }">
                   <el-link type="primary" :href="href" @click="navigate">
@@ -80,12 +88,16 @@
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
-import { getExperts } from '~/api/Expert'
-import { Expert } from '~/models/Expert'
 import { useStore } from '~/store/index'
+import { getHomepage } from '~/api/Homepage'
+import { Homepage } from '~/models/Homepage'
+import { Expert } from '~/models/Expert'
 const expertArr = ref<Expert[]>([])
+const homepage = ref<Homepage>()
 onMounted(async () => {
-  expertArr.value = await getExperts(8)
+  homepage.value = await getHomepage()
+  expertArr.value = homepage.value.top_experts
+  console.log(window.innerWidth)
 })
 // catch expert_id
 const store = useStore()
@@ -108,5 +120,20 @@ const getIdExpert = (id: string) => {
   border-radius: 20px;
   width: 220px;
   margin: 0 auto 40px auto;
+}
+.col:last-child {
+  display: none;
+}
+
+@media screen and (max-width: 1198.5px) {
+  .col:last-child {
+    display: block;
+  }
+}
+
+@media screen and (max-width: 990px) {
+  .col:last-child {
+    display: none;
+  }
 }
 </style>
